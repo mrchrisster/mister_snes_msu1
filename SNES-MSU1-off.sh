194d5a62b7f4e72ab0eddfa2f5d033c7cd5573a0
Marcelo Medeiros <marcelomedeirosb@gmail.com>
2022-04-08 17:35:57 -0300

    Update SNES-MSU1-off.sh

--- SNES-MSU1-off.sh
+++ SNES-MSU1-off.sh
@@ -17,7 +17,7 @@ fi
 
 rm /media/fat/MiSTer
 rm /media/fat/MiSTer.ini
-rm /media/fat/_Unstable_and_Old/SNES-MSU1.rbf
+rm /media/fat/_Console/SNES-MSU1.rbf
 mv /media/fat/MiSTer.bak /media/fat/MiSTer
 mv /media/fat/MiSTer.ini.bak /media/fat/MiSTer.ini
 reboot
